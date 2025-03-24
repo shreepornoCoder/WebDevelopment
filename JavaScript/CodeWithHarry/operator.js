bdce02209ed93b6b62fe6ba747c2f6ff9a7ee883
shreepornoCoder <sontos1979@gmail.com>
2025-03-24 19:32:13 +0600

    File Added

---
+++ JavaScript/CodeWithHarry/operator.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 4;
+
+console.log(a+b); // Addition
+console.log(a*b); // Substraction
+console.log(a%b); // Modulus
+console.log(a**b); // power/Exponent
+console.log(a%b); // Division
+
+// increment operator
+console.log(a++);
+console.log(++a);
+
+// Decrement
+console.log(a--);
+console.log(--a);
+
+// comparision operator
+console.log("a !== b", a!==b);
